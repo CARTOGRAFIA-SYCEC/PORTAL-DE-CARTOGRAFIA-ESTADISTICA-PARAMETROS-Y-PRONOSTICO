b8aef54c4ca1b95c889dab1734f2323dc5ee0eba
SYCEC.SIG <166184656+CARTOGRAFIA-SYCEC@users.noreply.github.com>
2024-04-12 11:10:06 -0600

    Update 0_PPARAMETROS.js

--- 0_PPARAMETROS.js
+++ 0_PPARAMETROS.js
@@ -1,4 +1,4 @@
-let popparameters = {
+let pparameters = {
 "type": "FeatureCollection",
 "name": "Poligono_Parametros",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
