de08daf56e8ef53f21a7bd3170c9ec384b743836
SYCEC.SIG <166184656+CARTOGRAFIA-SYCEC@users.noreply.github.com>
2024-06-11 15:32:03 -0600

    Add files via upload

---
+++ 0_BMT.js
@@ -0,0 +1,16 @@
+let bmt = {
+"type": "FeatureCollection",
+"name": "0_BMT",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NO": 12, "NOMBRE_BOYA": "BMT2-01-T80", "T_BOYA": "BMT2-01", "TG_BOYA": "BMT2", "N_BOYA": "Boya Metoceanica Tipo 2", "NG_BOYA": "Boya Metoceanica", "COORD_X": -92.275367, "COORD_Y": 19.551133 }, "geometry": { "type": "Point", "coordinates": [ -92.275367, 19.551133 ] } },
+{ "type": "Feature", "properties": { "NO": 13, "NOMBRE_BOYA": "BMT2-02-T45", "T_BOYA": "BMT2-02", "TG_BOYA": "BMT2", "N_BOYA": "Boya Metoceanica Tipo 2", "NG_BOYA": "Boya Metoceanica", "COORD_X": -93.1972, "COORD_Y": 18.750017 }, "geometry": { "type": "Point", "coordinates": [ -93.1972, 18.750017 ] } },
+{ "type": "Feature", "properties": { "NO": 14, "NOMBRE_BOYA": "BMT2-03-T1250", "T_BOYA": "BMT2-03", "TG_BOYA": "BMT2", "N_BOYA": "Boya Metoceanica Tipo 2", "NG_BOYA": "Boya Metoceanica", "COORD_X": -95.2292, "COORD_Y": 19.07015 }, "geometry": { "type": "Point", "coordinates": [ -95.2292, 19.07015 ] } },
+{ "type": "Feature", "properties": { "NO": 15, "NOMBRE_BOYA": "BMT2-04-T300", "T_BOYA": "BMT2-04", "TG_BOYA": "BMT2", "N_BOYA": "Boya Metoceanica Tipo 2", "NG_BOYA": "Boya Metoceanica", "COORD_X": -96.866017, "COORD_Y": 24.551125 }, "geometry": { "type": "Point", "coordinates": [ -96.866017, 24.551125 ] } },
+{ "type": "Feature", "properties": { "NO": 16, "NOMBRE_BOYA": "BMT2-05-T80", "T_BOYA": "BMT2-05", "TG_BOYA": "BMT2", "N_BOYA": "Boya Metoceanica Tipo 2", "NG_BOYA": "Boya Metoceanica", "COORD_X": -96.86735, "COORD_Y": 20.817616 }, "geometry": { "type": "Point", "coordinates": [ -96.86735, 20.817616 ] } },
+{ "type": "Feature", "properties": { "NO": 17, "NOMBRE_BOYA": "BMT2-06-T300", "T_BOYA": "BMT2-06", "TG_BOYA": "BMT2", "N_BOYA": "Boya Metoceanica Tipo 2", "NG_BOYA": "Boya Metoceanica", "COORD_X": -94.039866, "COORD_Y": 18.800583 }, "geometry": { "type": "Point", "coordinates": [ -94.039866, 18.800583 ] } },
+{ "type": "Feature", "properties": { "NO": 18, "NOMBRE_BOYA": "BMT2-07-T20", "T_BOYA": "BMT2-07", "TG_BOYA": "BMT2", "N_BOYA": "Boya Metoceanica Tipo 2", "NG_BOYA": "Boya Metoceanica", "COORD_X": -91.839933, "COORD_Y": 19.180233 }, "geometry": { "type": "Point", "coordinates": [ -91.839933, 19.180233 ] } },
+{ "type": "Feature", "properties": { "NO": 19, "NOMBRE_BOYA": "BMT3-01-T2500", "T_BOYA": "BMT3-01", "TG_BOYA": "BMT3", "N_BOYA": "Boya Metoceanica Tipo 3", "NG_BOYA": "Boya Metoceanica", "COORD_X": -95.505283, "COORD_Y": 19.872566 }, "geometry": { "type": "Point", "coordinates": [ -95.505283, 19.872566 ] } },
+{ "type": "Feature", "properties": { "NO": 20, "NOMBRE_BOYA": "BMT3-02-T2500", "T_BOYA": "BMT3-02", "TG_BOYA": "BMT3", "N_BOYA": "Boya Metoceanica Tipo 3", "NG_BOYA": "Boya Metoceanica", "COORD_X": -95.380067, "COORD_Y": 25.380033 }, "geometry": { "type": "Point", "coordinates": [ -95.380067, 25.380033 ] } }
+]
+}
