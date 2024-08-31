7d84176b9da0a627082731797fd588b5d5585c48
SYCEC.SIG <166184656+CARTOGRAFIA-SYCEC@users.noreply.github.com>
2024-08-31 13:13:29 -0600

    Update GP_OIL.js

--- Tematizacion/GP_OIL.js
+++ Tematizacion/GP_OIL.js
@@ -23,9 +23,9 @@ let esridark = L.tileLayer('https://services.arcgisonline.com/ArcGIS/rest/servic
 
 //  CONFIGURACIÓN LIENZO DEL MAPA //
 let map = L.map('map', {
-        layers: [esridark],
+        layers: [osm],
         tap: false,
-        center: new L.LatLng(19.5, -93),
+        center: new L.LatLng(20, -94),
         zoom: 8,
         minZoom: 5,
         fullscreenControl: true,
@@ -76,7 +76,7 @@ let miniMap = new L.Control.MiniMap(MMap, { toggleDisplay: true, position: 'bott
 let review = L.control.resetView({
         position: "topleft",
         title: "Return zoom",
-        latlng: L.latLng([19.5, -93]),
+        latlng: L.latLng([20, -94]),
         zoom: 8,
     }).addTo(map);
 
@@ -189,196 +189,44 @@ let ISA_PEP = L.geoJSON(ISA_2006, {style: function (feature){
     }).addTo(map);
 
 // GEOTIFF (CAPAS)
-let CargaTIFF1 = "./GeoTIFF/OS0701.tif";
+let CargaTIFF1 = "./GeoTIFF/Konen-1EXP_lluvias.tif";
 let RenderizadoTIFF1 = L.LeafletGeotiff.plotty({
-        displayMin: 1,
-        displayMax: 9,
+        displayMin: 5,
+        displayMax: 101,
         clampLow: false,
         clampHigh: false,
         colorScale: 'turbo',
     });
 let NombreTIFF1 = L.leafletGeotiff(CargaTIFF1, {
         renderer: RenderizadoTIFF1,
-        opacity: 0.75,
-    });
+        opacity: 0.90,
+    }).addTo(map);
 
-let CargaTIFF2 = "./GeoTIFF/OS0710.tif";
+let CargaTIFF2 = "./GeoTIFF/Konen-1EXP_nortes.tif";
 let RenderizadoTIFF2 = L.LeafletGeotiff.plotty({
-        displayMin: 1,
-        displayMax: 9,
+        displayMin: 5,
+        displayMax: 101,
         clampLow: false,
         clampHigh: false,
         colorScale: 'turbo',
     });
 let NombreTIFF2 = L.leafletGeotiff(CargaTIFF2, {
         renderer: RenderizadoTIFF2,
-        opacity: 0.75,
-    });
+        opacity: 0.90,
+    }).addTo(map);
 
-let CargaTIFF3 = "./GeoTIFF/OS0720.tif";
+let CargaTIFF3 = "./GeoTIFF/Konen-1EXP_secas.tif";
 let RenderizadoTIFF3 = L.LeafletGeotiff.plotty({
-        displayMin: 1,
-        displayMax: 9,
+        displayMin: 5,
+        displayMax: 101,
         clampLow: false,
         clampHigh: false,
         colorScale: 'turbo',
     });
 let NombreTIFF3 = L.leafletGeotiff(CargaTIFF3, {
         renderer: RenderizadoTIFF3,
-        opacity: 0.75,
-    });
-
-let CargaTIFF4 = "./GeoTIFF/OS0730.tif";
-let RenderizadoTIFF4 = L.LeafletGeotiff.plotty({
-        displayMin: 1,
-        displayMax: 9,
-        clampLow: false,
-        clampHigh: false,
-        colorScale: 'turbo',
-    });
-let NombreTIFF4 = L.leafletGeotiff(CargaTIFF4, {
-        renderer: RenderizadoTIFF4,
-        opacity: 0.75,
-    });
-
-let CargaTIFF5 = "./GeoTIFF/OS0810.tif";
-let RenderizadoTIFF5 = L.LeafletGeotiff.plotty({
-        displayMin: 1,
-        displayMax: 9,
-        clampLow: false,
-        clampHigh: false,
-        colorScale: 'turbo',
-    });
-let NombreTIFF5 = L.leafletGeotiff(CargaTIFF5, {
-        renderer: RenderizadoTIFF5,
-        opacity: 0.75,
-    });
-
-let CargaTIFF6 = "./GeoTIFF/OS0820.tif";
-let RenderizadoTIFF6 = L.LeafletGeotiff.plotty({
-        displayMin: 1,
-        displayMax: 9,
-        clampLow: false,
-        clampHigh: false,
-        colorScale: 'turbo',
-    });
-let NombreTIFF6 = L.leafletGeotiff(CargaTIFF6, {
-        renderer: RenderizadoTIFF6,
-        opacity: 0.75,
-    });
-
-let CargaTIFF7 = "./GeoTIFF/OS0830.tif";
-let RenderizadoTIFF7 = L.LeafletGeotiff.plotty({
-        displayMin: 1,
-        displayMax: 9,
-        clampLow: false,
-        clampHigh: false,
-        colorScale: 'turbo',
-    });
-let NombreTIFF7 = L.leafletGeotiff(CargaTIFF7, {
-        renderer: RenderizadoTIFF7,
-        opacity: 0.75,
-    });
-
-let CargaTIFF8 = "./GeoTIFF/OS0910.tif";
-let RenderizadoTIFF8 = L.LeafletGeotiff.plotty({
-        displayMin: 1,
-        displayMax: 9,
-        clampLow: false,
-        clampHigh: false,
-        colorScale: 'turbo',
-    });
-let NombreTIFF8 = L.leafletGeotiff(CargaTIFF8, {
-        renderer: RenderizadoTIFF8,
-        opacity: 0.75,
-    });
-    
-
-let CargaTIFF9 = "./GeoTIFF/OS0920.tif";
-let RenderizadoTIFF9 = L.LeafletGeotiff.plotty({
-        displayMin: 1,
-        displayMax: 9,
-        clampLow: false,
-        clampHigh: false,
-        colorScale: 'turbo',
-    });
-let NombreTIFF9 = L.leafletGeotiff(CargaTIFF9, {
-        renderer: RenderizadoTIFF9,
-        opacity: 0.75,
-    });
-
-let CargaTIFF10 = "./GeoTIFF/OS0930.tif";
-let RenderizadoTIFF10 = L.LeafletGeotiff.plotty({
-        displayMin: 1,
-        displayMax: 9,
-        clampLow: false,
-        clampHigh: false,
-        colorScale: 'turbo',
-    });
-let NombreTIFF10 = L.leafletGeotiff(CargaTIFF10, {
-        renderer: RenderizadoTIFF10,
-        opacity: 0.75,
-    });
-
-let CargaTIFF11 = "./GeoTIFF/OS1005.tif";
-let RenderizadoTIFF11 = L.LeafletGeotiff.plotty({
-        displayMin: 1,
-        displayMax: 9,
-        clampLow: false,
-        clampHigh: false,
-        colorScale: 'turbo',
-    });
-let NombreTIFF11 = L.leafletGeotiff(CargaTIFF11, {
-        renderer: RenderizadoTIFF11,
-        opacity: 0.75,
-    });
-
-let CargaTIFF12 = "./GeoTIFF/OS1015.tif";
-let RenderizadoTIFF12 = L.LeafletGeotiff.plotty({
-        displayMin: 1,
-        displayMax: 9,
-        clampLow: false,
-        clampHigh: false,
-        colorScale: 'turbo',
-    });
-let NombreTIFF12 = L.leafletGeotiff(CargaTIFF12, {
-        renderer: RenderizadoTIFF12,
-        opacity: 0.75,
+        opacity: 0.90,
     }).addTo(map);
-
-let CargaTIFF13 = "./GeoTIFF/OSTUMUT_V.tif";
-let RenderizadoTIFF13 = L.LeafletGeotiff.plotty({
-        displayMin: 1,
-        displayMax: 10,
-        clampLow: false,
-        clampHigh: false,
-        colorScale: 'turbo',
-    });
-let NombreTIFF13 = L.leafletGeotiff(CargaTIFF13, {
-        renderer: RenderizadoTIFF13,
-        opacity: 0.75,
-    });
-    
-let CargaTIFF14 = "./GeoTIFF/OSTUMUT_P.tif";
-let RenderizadoTIFF14 = L.LeafletGeotiff.plotty({
-        displayMin: 1,
-        displayMax: 13,
-        clampLow: false,
-        clampHigh: false,
-        colorScale: 'turbo',
-    });
-let NombreTIFF14 = L.leafletGeotiff(CargaTIFF14, {
-        renderer: RenderizadoTIFF14,
-        opacity: 0.75,
-    });
-
-// WEB MAP SERVICE (WMS - CAPAS) //
-//let ActiveCTH = L.tileLayer.wms("https://nowcoast.noaa.gov/geoserver/hazards/tropical_cyclones/ows?", {
-//        layers: 'active_tropical_cyclones',
-//        format: 'image/png',
-//        transparent: true,
-//        attribution: "NOAA nowCOAST"
-//    });
 
 // ACTIVAR CAPAS //
 let baseLayers = {
@@ -391,22 +239,10 @@ let baseLayers = {
         'ESRI DarkMap': esridark,
     };               
 let overlays = {
-//        'Ciclon Tropical / Huracán activo':ActiveCTH,
         'Índice de Sensibilidad Ambiental - ISA': ISA_PEP,
-        'Oil Spill - Concentración de eventos':NombreTIFF13,
-        'Oil Spill - Porcentaje':NombreTIFF14,
-        'Oil Spill - 01/07/2010':NombreTIFF1,
-        'Oil Spill - 10/07/2010':NombreTIFF2,
-        'Oil Spill - 20/07/2010':NombreTIFF3,
-        'Oil Spill - 30/07/2010':NombreTIFF4,
-        'Oil Spill - 10/08/2010':NombreTIFF5,
-        'Oil Spill - 20/08/2010':NombreTIFF6,
-        'Oil Spill - 30/08/2010':NombreTIFF7,
-        'Oil Spill - 10/09/2010':NombreTIFF8,
-        'Oil Spill - 20/09/2010':NombreTIFF9,
-        'Oil Spill - 30/09/2010':NombreTIFF10,
-        'Oil Spill - 05/10/2010':NombreTIFF11,
-        'Oil Spill - 15/10/2010':NombreTIFF12,
+        'Konen-1EXP - Nortes':NombreTIFF2,
+        'Konen-1EXP - Lluvias':NombreTIFF1,
+        'Konen-1EXP - Secas':NombreTIFF3,
     };
 let layerControl = L.control.layers(
             baseLayers,
